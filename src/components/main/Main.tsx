1521636e76ebae96196c26b24072e3483b8b3c36
TomaszBeben <tbeben@st.swps.edu.pl>
2022-02-15 14:55:07 +0100

    first commit

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Content from '../content/Content'
+import Navigation from '../navigation/Navigation'
+
+const Main = () => {
+  return (
+    <div>
+        <Navigation/>
+        <Content/>
+    </div>
+  )
+}
+
+export default Main
